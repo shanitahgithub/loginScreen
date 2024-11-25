6e7e7035da2332e601415efa945f05ab8e1c9146
shanitahgithub <nakaggshanitah874@gmail.com>
2024-11-25 20:25:04 +0300

    signup Scree

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../screens/loginScreen.dart';
+import 'screens/signUp.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: SignupPage(),
       // theme: ThemeData.light(),
     );
   }
